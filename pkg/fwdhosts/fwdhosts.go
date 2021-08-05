004400b8ad7edbc2cd9de3617d4e461f76268633
Robb Lovell <robb@codezero.io>
2021-08-05 11:35:04 -0700

    removed finished todos and commented code, corrected packing ref at line 1

--- pkg/fwdhosts/fwdhosts.go
+++ pkg/fwdhosts/fwdhosts.go
@@ -1,4 +1,4 @@
-//go:generate ${gopath}/bin/mockgen -source=fwdport.go -destination=mock_fwdport.go -package=fwdport
+//go:generate ${gopath}/bin/mockgen -source=fwdhosts.go -destination=mock_fwdhosts.go -package=fwdhosts
 package fwdhosts
 
 import (
@@ -13,7 +13,6 @@ import (
 // HostFileWithLock
 type HostFileWithLock struct {
     Hosts *txeh.Hosts
-    //HostStrings []string
     sync.Mutex
 }
 
@@ -31,12 +30,6 @@ type HostModifierOpts struct {
 
 func (params *HostModifierOpts) AddHosts() {
 
-    // todo: this check needs to be done outside.
-    // We must not add multiple hosts entries for different ports on the same service
-    //if operator.Pfo.getBrothersInPodsAmount() != 1 {
-    //    return
-    //}
-
     params.HostFile.Lock()
     // pfo.Service holds only the service name
     // start with the smallest allowable hostname
@@ -44,7 +37,6 @@ func (params *HostModifierOpts) AddHosts() {
     // bare service name
     if params.ClusterN == 0 && params.NamespaceN == 0 {
         params.addHost(params.Service)
-
         if params.Domain != "" {
             params.addHost(fmt.Sprintf(
                 "%s.%s",
@@ -91,7 +83,6 @@ func (params *HostModifierOpts) AddHosts() {
                params.Domain,
            ))
        }
-
     }
 
     params.addHost(fmt.Sprintf(
@@ -124,12 +115,6 @@ func (params *HostModifierOpts) AddHosts() {
 
 // RemoveHosts removes hosts /etc/hosts  associated with a forwarded pod
 func (params *HostModifierOpts) RemoveHosts() {
-    // todo: this check needs to be done outside.
-    // We must not remove hosts entries if port-forwarding on one of the service ports is cancelled and others not
-    //if operator.Pfo.getBrothersInPodsAmount() > 0 {
-    //    return
-    //}
-
     // we should lock the pfo.HostFile here
     // because sometimes other goroutine write the *txeh.Hosts
     params.HostFile.Lock()
@@ -148,7 +133,6 @@ func (params *HostModifierOpts) RemoveHosts() {
         params.HostFile.Hosts.RemoveHost(host)
     }
 
-    // fmt.Printf("Delete Host And Save !\r\n")
     err = params.HostFile.Hosts.Save()
     if err != nil {
         log.Errorf("Error saving /etc/hosts: %s\n", err.Error())
@@ -164,13 +148,8 @@ func (params *HostModifierOpts) addHost(host string) {
     // add to list of hostnames for this port-forward
     params.Hosts = append(params.Hosts, host)
 
-    // todo: check to see if this needs to be called:
-    //  //if operator.Pfo.getBrothersInPodsAmount() > 0 {
-    //    //    return
-    //    //}
     // remove host if it already exists in /etc/hosts
     params.HostFile.Hosts.RemoveHost(host)
-    //params.HostFile.HostStrings = append(params.HostFile.HostStrings, host)
 
     // add host to /etc/hosts
     params.HostFile.Hosts.AddHost(params.LocalIp.String(), host)
