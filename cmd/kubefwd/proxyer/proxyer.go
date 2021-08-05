dc20b0d2c5c258911c69910384853aa3166ca56f
Grant Burnard <grant@burnard.com>
2021-08-05 11:15:19 -0700

    Additional log message for proxy

--- cmd/kubefwd/proxyer/proxyer.go
+++ cmd/kubefwd/proxyer/proxyer.go
@@ -12,6 +12,7 @@ func Proxyer(localIp string, localPort int32, remoteIp string, remotePort int32)
 	localAddress := fmt.Sprintf("%s:%d", localIp, localPort)
 	remoteAddress := fmt.Sprintf("%s:%d", remoteIp, remotePort)
 	listener, err := net.Listen("tcp", localAddress)
+	log.Infof("Proxyer: Setting up proxy from %s to %s", localAddress, remoteAddress)
 	if err != nil {
 		log.Errorf("Proxyer: Proxy conflict: ", err)
 		return
